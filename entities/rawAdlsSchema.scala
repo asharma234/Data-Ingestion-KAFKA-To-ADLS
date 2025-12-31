62bc37e9b2ed000cb555c832e83b4ffdf2b9ca70
asharma234 <109665710+asharma234@users.noreply.github.com>
2025-12-31 11:35:17 +0400

    Add rawAdlsSchema

---
+++ entities/rawAdlsSchema.scala
@@ -0,0 +1,13 @@
+package xxx.xxx.xxx.entities
+
+case class xxxMinSchema(
+                                   customer_id: String,
+                                   message_time: String,
+                                   message_uuid: String,
+                                   message_source: String,
+                                   message_name: String,
+                                   message_format: String,
+                                   created_date: String,
+                                   created_date_time: String,
+                                   changed_date_time: String
+					   )
